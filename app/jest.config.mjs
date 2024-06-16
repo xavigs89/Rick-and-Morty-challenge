76bdcd99055d8160ad02ddd1b53f49a148654d86
xavigs89 <xavigs1989@gmail.com>
2024-06-16 11:59:15 +0200

    install all dependencies to use jest for frontend tests

---
+++ app/jest.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
